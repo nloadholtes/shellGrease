061d50b6b7bbe7e71bab7e9cd843a49dcf63ccb5
Nick Loadholtes <nick@ironboundsoftware.com>
2011-11-03 14:53:51 -0400

    Initial checkin Adding a script to automate git fetches

---
+++ README.md
@@ -0,0 +1,2 @@
+# Shell Grease
+This is a collection of various scripts to make things slicker while working in a shell.
---
+++ fetch_git.sh
@@ -0,0 +1,10 @@
+#
+# fetch_git.sh
+# Nick Loadholtes <nick@ironboundsoftware.com>
+# Nov 3, 2011
+#
+# Looks at the directories in the current dir
+# and if any of them contain a .git dir it will
+# attempt to go a 'git fetch' to pull down anything.
+#
+for x in `ls` ; do if [ -d $x/.git ]; then echo "--->Checking $x"; cd $x; git fetch; cd ..; fi; done
