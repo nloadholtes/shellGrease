1c90502163cc7fd8a71341232de356fc0857d2aa
Nick Loadholtes <nick@ironboundsoftware.com>
2011-11-14 11:14:31 -0500

    Adding an svn update script

---
+++ update_svn.sh
@@ -0,0 +1,10 @@
+#
+# Goes through each of the sub directories and runs
+# svn up
+# to update the projects. This script should be run
+# manually to let you handle any merge/conflit issues
+# that might crop up.
+#
+# Nick Loadholtes, 2/14/2011
+
+for x in `ls`;  do echo $x; cd $x; svn up; cd ..; done;
