9b1eec0e89b57e3f2d630a32c852bb7f3cbe5317
Nick Loadholtes <nick@ironboundsoftware.com>
2011-12-29 23:00:45 -0500

    Adding a script to do mass pulls for mercurial

---
+++ pull_hg.sh
@@ -0,0 +1,10 @@
+#
+# pull_hg.sh
+# Nick Loadholtes <nick@ironboundsoftware.com>
+# Dec 29, 2011
+#
+# Looks at the directories in the current dir
+# and if any of them contain a .hg dir it will
+# attempt to go a 'hg pull' to pull down anything.
+#
+for x in `ls` ; do if [ -d $x/.hg ]; then echo "--->Checking $x"; cd $x; hg pull; cd ..; fi; done
