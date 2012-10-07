3499d5dfe987e03ba700ad7f0ac0fe8ab44486a5
Nick Loadholtes <nick@ironboundsoftware.com>
2012-10-07 17:53:04 -0400

    First pass, needs much better option checking

--- tell_me_about_it.sh
+++ tell_me_about_it.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import subprocess
 
 MAIL_TO = ""
 MAIL_FROM = ""
@@ -8,5 +9,18 @@ MAIL_FROM = ""
 def _send_email(subject, body, emailto=MAIL_TO, emailfrom=MAIL_FROM):
     return None
 
+def _startjob(args):
+    """Start up a job"""
+    print(args)
+    ret_val = subprocess.call(args)
+    return ret_val
+
+
+
 if __name__ == "__main__":
-    print("Hello")
+    if len(sys.argv) < 2:
+        print("Need a command to run")
+        exit(1)
+
+    args = sys.argv[1:]
+    print(_startjob(args))
