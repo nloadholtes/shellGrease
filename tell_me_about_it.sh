a0139c069e07b4d51317309380b9e608ca8c35d7
Nick Loadholtes <nick@ironboundsoftware.com>
2012-10-07 17:29:01 -0400

    A script that can execute shell commands, and email someone when it is done.

---
+++ tell_me_about_it.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+
+MAIL_TO = ""
+MAIL_FROM = ""
+
+def _send_email(subject, body, emailto=MAIL_TO, emailfrom=MAIL_FROM):
+    return None
+
+if __name__ == "__main__":
+    print("Hello")
