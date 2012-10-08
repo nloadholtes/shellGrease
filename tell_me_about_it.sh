36831d7ed4525ad45b0f29c3b127dc99485af07c
Nick Loadholtes <nick@ironboundsoftware.com>
2012-10-07 22:49:32 -0400

    Mail section needed improvements

--- tell_me_about_it.sh
+++ tell_me_about_it.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
 
 import sys
+import os
 import subprocess
+from ConfigParser import SafeConfigParser
 
 MAIL_TO = ""
 MAIL_FROM = ""
 
-def _send_email(subject, body, emailto=MAIL_TO, emailfrom=MAIL_FROM):
-    return None
+def _send_email(subject, body, parser):
+    MAIL_TO = parser.get("MAIL", "mail_to")
+    MAIL_FROM = parser.get("MAIL", "mail_from")
+    MAIL_SERVER = parser.get("MAIL", "mail_server")
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = MAIL_FROM
+    msg['To'] = MAIL_TO
+
+    msg.attach(MIMEText(body, 'plain'))
+    print msg
+    smtp = smtplib.SMTP(parser.get("MAIL", "mail_server"),
+                        parser.get("MAIL", "mail_port"))
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login(parser.get("MAIL", "login"), parser.get("MAIL", "password"))
+    smtp.sendmail('<Job finished notification>%s' % MAIL_FROM,
+                  MAIL_TO, msg.as_string())
+    smtp.close()
 
 def _startjob(args):
     """Start up a job"""
@@ -16,11 +36,13 @@ def _startjob(args):
     return ret_val
 
 
-
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Need a command to run")
         exit(1)
 
+    parser = SafeConfigParser()
+    parser.read(os.path.expanduser('~/shellGrease.ini'))
     args = sys.argv[1:]
     print(_startjob(args))
+    _send_email("Quick test", _startjob(args), parser)
