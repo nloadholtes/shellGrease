4808a16f9ed0eda8c30dd429e94978b46871e6cb
Nick Loadholtes <nick@ironboundsoftware.com>
2013-04-20 23:03:49 -0400

    I don't know why I didn't have this one for git already

---
+++ stats_git.sh
@@ -0,0 +1,5 @@
+#
+# Shows the top ten commands you are
+# using in git
+#
+history|grep git| awk '{CMD[$3]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n10
