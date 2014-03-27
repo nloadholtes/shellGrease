ed2a66f66b0c423703c1a2e378888965fc33ca3d
Nick Loadholtes <nick@ironboundsoftware.com>
2014-03-27 10:39:01 -0400

    Adding a helper to do git pulls

---
+++ pull_git.sh
@@ -0,0 +1,13 @@
+#
+# fetch_git.sh
+# Nick Loadholtes <nick@ironboundsoftware.com>
+# March 27, 2014
+#
+# Looks at the directories in the current dir
+# and if any of them contain a .git dir it will
+# attempt to go a 'git pull' to pull down anything.
+#
+# The idea is this can be used to quickly bring everything
+# up to date as quickly as possible.
+#
+for x in $(ls) ; do if [ -d $x/.git ]; then echo "--->Checking $x"; cd $x; git pull; cd ..; fi; done
