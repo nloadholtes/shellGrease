368c3587a0915afdc2916f23c7d3d502da564aa4
Nick Loadholtes <nick@ironboundsoftware.com>
2015-05-04 10:43:29 -0400

    Cleanup

--- take_picture.sh
+++ take_picture.sh
@@ -8,7 +8,6 @@ if [[ $(uname) == 'Linux' ]]; then
   streamer -q -s 2560x1440 -o ~/commit_pics/nick-$(date +%Y_%m_%d-%H_%M_%S).jpeg
 else
   # if osx
-  # to do
   echo "Running on a mac?"
   imagesnap
 fi
