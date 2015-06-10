3f1af06ddd0ebebf583df20fcb28984234667696
Nick Loadholtes <nick@ironboundsoftware.com>
2015-06-10 11:53:22 -0400

    Making the imagesnap stuff work

--- take_picture.sh
+++ take_picture.sh
@@ -8,6 +8,6 @@ if [[ $(uname) == 'Linux' ]]; then
   streamer -q -s 2560x1440 -o ~/commit_pics/nick-$(date +%Y_%m_%d-%H_%M_%S).jpeg
 else
   # if osx
-  echo "Running on a mac?"
-  imagesnap
+  #echo "Running on a mac?"
+  imagesnap -q ~/commit_pics/$USER_$(date +%y_%m_%d-%H_%M_%S).jpg
 fi
