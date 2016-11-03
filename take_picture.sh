db39904e5e1961a8088c1fa64184951786e42312
Nick Loadholtes <nick@ironboundsoftware.com>
2016-11-03 14:50:08 -0400

    Updating the take a picture script

--- take_picture.sh
+++ take_picture.sh
@@ -5,7 +5,8 @@
 #
 # if linux
 if [[ $(uname) == 'Linux' ]]; then
-  streamer -q -s 2560x1440 -o ~/commit_pics/nick-$(date +%Y_%m_%d-%H_%M_%S).jpeg
+    #  streamer -q -s 2560x1440 -o ~/commit_pics/nick-$(date +%Y_%m_%d-%H_%M_%S).jpeg
+    fswebcam --no-banner -q ~/commit_pics/nick-$(date +%Y_%m_%d-%H_%M_%S).jpeg -F 5
 else
   # if osx
   #echo "Running on a mac?"
